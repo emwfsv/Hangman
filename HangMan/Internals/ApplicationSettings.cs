58a2cf39d9a023ac9aeb6592c6118daa7579bacf
emwfsv <emwfsv@gmail.com>
2023-03-10 09:52:38 +0100

    Updated graphical changes

--- HangMan/Internals/ApplicationSettings.cs
+++ HangMan/Internals/ApplicationSettings.cs
@@ -16,6 +16,7 @@ namespace HangMan.Internals
                 new HangmanAnatomy() { AnatomyLineNumber = 0, AnatomyName = "Pole", LineStartPoint = new Point(0 + centerOffsetWidth, centerOffsetBottom), LineEndPoint = new Point(0 + centerOffsetWidth, (centerOffsetBottom / 4) * 1) },
                 new HangmanAnatomy() { AnatomyLineNumber = 1, AnatomyName = "UpperPole", LineStartPoint = new Point(0 + centerOffsetWidth, (centerOffsetBottom / 4) * 1), LineEndPoint = new Point(centerOffsetWidth + centerOffsetWidth / 2, (centerOffsetBottom / 4) * 1) },
                 new HangmanAnatomy() { AnatomyLineNumber = 2, AnatomyName = "Support", LineStartPoint = new Point(0 + centerOffsetWidth, (centerOffsetBottom / 4) * 2), LineEndPoint = new Point(centerOffsetWidth + centerOffsetWidth / 3, (centerOffsetBottom / 4) * 1) },
+                new HangmanAnatomy() { AnatomyLineNumber = 3, AnatomyName = "Rope", LineStartPoint = new Point(centerOffsetWidth + centerOffsetWidth / 2, (centerOffsetBottom / 4) * 1), LineEndPoint = new Point(centerOffsetWidth + centerOffsetWidth / 2, (centerOffsetBottom / 4) * 2) },
             };
 
             return returnList;
